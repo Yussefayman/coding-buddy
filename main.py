8d7215311921576a089fc3bfb1697633fc349853
Yussef Ayman <yusssefayman98@gmail.com>
2026-02-21 17:42:01 +0300

    main

---
+++ main.py
@@ -0,0 +1,145 @@
+from pathlib import Path
+import sys
+from typing import Any
+import asyncio
+import click
+from config.config import Config
+from dotenv import load_dotenv
+
+
+from agent.agent import Agent
+from agent.events import AgentEventType
+from config.loader import load_config
+from ui.tui import TUI, get_console
+
+load_dotenv()
+console = get_console()
+
+
+class CLI:
+    def __init__(self,config: Config) -> None:
+        self.agent: Agent | None = None
+        self.tui = TUI(config=config,console=console)
+        self.config = config
+
+    async def run_single(self,message)-> str | None:
+        async with Agent(self.config) as agent:
+            self.agent = agent
+            return await self._process_message(message)
+    
+    async def run_interactive(self)-> str | None:
+        self.tui.print_welcome('AI Agent', lines= [f'model: {self.config.model_name}',
+                                                   f'cwd: {self.config.cwd}',
+                                                   f'commands: /help /config /approval /model /exit',],)
+        async with Agent(self.config) as agent:
+            self.agent = agent
+            while True:
+                try:
+                    user_input = console.input("\n[user]>[/user] ").strip()
+                    if not user_input:
+                        continue
+                    
+                    await self._process_message(user_input)
+                    
+                except KeyboardInterrupt:
+                    console.print("\n[dim]Use /exit to quit[/dim]")
+                except EOFError:
+                    break
+
+        console.print("\n [dim]Goodbye![/dim]")   
+    
+    def _get_tool_kind(self, tool_name: str) -> str | None:
+        tool_kind = None
+        tool = self.agent.session.tool_registry.get(tool_name)
+        if not tool:
+            tool_kind = None
+            
+        tool_kind = tool.kind.value
+
+        return tool_kind
+
+    
+    async def _process_message(self, message: str) -> str | None:
+        if not self.agent:
+            return None
+        #ui start
+        assistant_streaming = False
+        final_response: str | None = None
+        
+        async for event in self.agent.run(message):
+            if event.type == AgentEventType.TEXT_DELTA:
+                content = event.data.get("content","")
+                if not assistant_streaming:
+                    self.tui.begin_assitant()
+                    assistant_streaming = True
+                self.tui.stream_assistant_delta(content)
+                
+            elif event.type == AgentEventType.TEXT_COMPLETE:
+                final_response = event.data.get("content")
+                if assistant_streaming:
+                    self.tui.end_assitant()
+                    assistant_streaming = False
+        
+            elif event.type == AgentEventType.AGENT_ERROR:
+                error = event.data.get("error","Unknown error")
+                console.print(f"\n [error]Error: {error}[/error]")
+            
+            elif event.type == AgentEventType.TOOL_CALL_START:
+                tool_name = event.data.get("name","unknown")
+                tool_kind = self._get_tool_kind(tool_name=tool_name)
+
+                self.tui.tool_call_start(
+                    event.data.get('call_id',''),
+                    tool_name,
+                    tool_kind,
+                    event.data.get('arguments',{}),
+                )
+            elif event.type == AgentEventType.TOOL_CALL_COMPLETE:
+                tool_name = event.data.get("name","unknown")
+                tool_kind = self._get_tool_kind(tool_name=tool_name)
+                self.tui.tool_call_complete(
+                    event.data.get('call_id',''),
+                    tool_name,
+                    tool_kind,
+                    event.data.get('success',False),
+                    event.data.get('output',""),
+                    event.data.get('error'),
+                    event.data.get('metadata'),
+                    event.data.get('truncated',False),
+                )
+
+        return final_response
+
+@click.command()
+@click.argument('prompt', required=False)
+@click.option('--cwd',
+              '-c',type=click.Path(exists=True, file_okay=False, path_type=Path),
+              help='Current Working Directory',
+)
+def main(prompt: str | None,
+         cwd: Path| None):
+    
+    try:
+        config = load_config(cwd = cwd)
+    except Exception as e:
+        console.print(f"[error]Configuration Error: {e}")
+        sys.exit(1)
+    errors = config.validate()
+    if errors:
+        for error in errors:
+            console.print(f"[error]{error}")
+        sys.exit(1)
+    
+    cli = CLI(config)
+
+    if prompt:
+        result = asyncio.run(cli.run_single(prompt))
+        if result is None:
+            sys.exit(1)
+    
+    else:
+        asyncio.run(cli.run_interactive())
+
+
+if __name__ == "__main__":
+    main()
