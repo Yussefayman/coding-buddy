4efe143b15afabd4c10d6e01f711061edae9747b
Yussef Ayman <yussefayman98@gmail.com>
2026-03-03 22:19:28 +0300

    web search tool

--- tools/builtin/__init__.py
+++ tools/builtin/__init__.py
@@ -4,9 +4,10 @@ from tools.builtin.grep import GrepTool
 from tools.builtin.list_dir import ListDirTool
 from tools.builtin.read_file import ReadFileTool
 from tools.builtin.shell import ShellTool
+from tools.builtin.web_search import WebSearchTool
 from tools.builtin.write_file import WriteFileTool
 
-_all__ = ['ReadFileTool','WriteFileTool','EditFileTool','ShellTool','ListDirTool','GrepTool','GlobTool']
+_all__ = ['ReadFileTool','WriteFileTool','EditFileTool','ShellTool','ListDirTool','GrepTool','GlobTool','WebSearchTool']
 
 def get_all_builtin_tools() -> list[type]:
     return [
@@ -16,5 +17,6 @@ def get_all_builtin_tools() -> list[type]:
         ShellTool,
         ListDirTool,
         GrepTool,
-        GlobTool
+        GlobTool,
+        WebSearchTool
     ]
